ca1d533a044b00e06ec99a4202653b7c419b555c
brenodss <brenondss@gmail.com>
2022-07-14 22:07:58 -0300

    Requisito 11 estruturado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './componentes/Login';
 import Provider from './context/myProvider';
+import Search from './componentes/SearchBar';
 
 function App() {
   return (
     <Provider>
       <Route exact path="/" component={ Login } />
+      <Search />
     </Provider>
   );
 }
--- src/componentes/SearchBar.js
+++ src/componentes/SearchBar.js
@@ -1,20 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types';
 // import context from '../context/myContext';
 
-const search = () => (
-  <>
-    <label htmlFor="ingredient">
-      <input id="ingredient" data-testid="ingredient-search-radio" type="radio" />
-    </label>
-    <label htmlFor="name">
-      <input id="name" data-testid="name-search-radio" type="radio" />
-    </label>
-    <label htmlFor="firstletter">
-      <input id="firstletter" data-testid="first-letter-search-radio" type="radio" />
-    </label>
-    <button data-testid="exec-search-btn" type="button">Buscar</button>
-  </>
-);
+// https://www.themealdb.com/api/json/v1/1/filter.php?i={ingrediente}
+// https://www.themealdb.com/api/json/v1/1/search.php?s={nome}
+// https://www.themealdb.com/api/json/v1/1/search.php?f={primeira-letra}
+// https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken
+// www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
 
-export default search;
+const Search = () => {
+  const [radio, setRadio] = useState('');
+  const [foods, setFoods] = useState({});
+
+  const simulation = {
+    nome: 'Arrabiata',
+    firstLetter: 'a',
+    ingredient: 'chicken_breast',
+  };
+
+  const handleRadios = ({ target }) => {
+    setRadio(target.value);
+  };
+
+  const handleFetchs = async (url) => {
+    const input = 'a';
+    if (input.length !== 1 && radio === 'FirstLetter') {
+      global.alert('Your search must have only 1 (one) character');
+      return;
+    }
+    const getResults = await fetch(url);
+    const data = await getResults.json();
+    setFoods(data);
+  };
+
+  const handleButton = async () => {
+    const ingred = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast';
+    const nome = `https://www.themealdb.com/api/json/v1/1/search.php?s=${simulation.nome}`;
+    const firstLetter = `https://www.themealdb.com/api/json/v1/1/search.php?f=${simulation.firstLetter}`;
+    switch (radio) {
+    case 'Ingredient':
+      handleFetchs(ingred);
+      break;
+    case 'Name':
+      handleFetchs(nome);
+      break;
+    case 'FirstLetter':
+      handleFetchs(firstLetter);
+      break;
+    default:
+      return fetch;
+    }
+  };
+
+  return (
+    <>
+      <label htmlFor="ingredient">
+        Ingredient
+        <input
+          value="Ingredient"
+          checked={ (radio === 'Ingredient') }
+          onChange={ handleRadios }
+          id="ingredient"
+          data-testid="ingredient-search-radio"
+          type="radio"
+        />
+      </label>
+
+      <label htmlFor="name">
+        Name
+        <input
+          value="Name"
+          checked={ (radio === 'Name') }
+          onChange={ handleRadios }
+          id="name"
+          data-testid="name-search-radio"
+          type="radio"
+        />
+      </label>
+
+      <label htmlFor="firstletter">
+        First Letter
+        <input
+          value="FirstLetter"
+          checked={ radio === 'FirstLetter' }
+          onChange={ handleRadios }
+          id="firstletter"
+          data-testid="first-letter-search-radio"
+          type="radio"
+        />
+      </label>
+      <button
+        onClick={ handleButton }
+        data-testid="exec-search-btn"
+        type="button"
+      >
+        Buscar
+      </button>
+    </>
+  );
+};
+
+export default Search;
