d5177e3e4670aeb1b242c67d6072dbc3ea124169
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-07-20 14:52:58 -0300

    implementa a pagina de profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../componentes/Footer';
 import Header from '../componentes/Header';
 
-const Profile = () => (
-  <div>
-    <h1>Profile</h1>
-    <Header title="Profile" />
-    <Footer />
-  </div>
-);
+const Profile = () => {
+  const history = useHistory();
+  return (
 
+    <div>
+      <h1>Profile</h1>
+      <Header title="Profile" />
+      <div>
+        <label htmlFor="email">
+          <input data-testid="profile-email" id="email" />
+        </label>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ () => {
+            localStorage.clear();
+            history.push('/');
+          } }
+        >
+          Logout
+        </button>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 export default Profile;
